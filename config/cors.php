0d8a2cbb800082c4f929155f317f03170669d5bc
Sirawit Tantiparinyakul <sirawit.tanti@gmail.com>
2026-06-16 16:00:30 +0700

    fixed: change config for vercel app

--- config/cors.php
+++ config/cors.php
@@ -21,7 +21,7 @@ return [
 
     'allowed_origins' => [
         'http://localhost:5173',
-        'https://your-frontend.vercel.app',
+        'https://finance-web-steel.vercel.app/',
     ],
 
     'allowed_origins_patterns' => [],
